c59074e610b01ed2767adcfaaf89834b11e810a2
Carol <carolinevictoria1224@gmail.com>
2026-04-17 15:46:34 -0300

    Lista de exercicios de laços de repetição

--- praticas/lacosRepeticao.js
+++ praticas/lacosRepeticao.js
@@ -34,9 +34,42 @@ for (let i = 0; i < senha.length; i++) {
 //5. Solicitando nomes até digitar fim
 console.log("5. Solicitando nomes até digitar fim")
 const entradas = ["ana", "carla", "manoel", "fim", "julia"];
-let i = 0;
+let b = 0;
 
-while (entradas[i] !== "fim"){
-    console.log("Nome: ", entradas[i]);
-    i++
+while (entradas[b] !== "fim") {
+    console.log("Nome: ", entradas[b]);
+    b++
+}
+
+//6. Número proibido
+console.log("6. Número proibido")
+for (a = 1; a <= 20; a++) {
+    if (a === 10) {
+        console.log("Número proibido encontrado! Encerrando...")
+        break;
+    }
+    console.log(a);
+}
+
+//7. Validação de Login
+console.log("7. Validação de Login")
+const tentativas = ["1234", "admin", "secreto"];
+const senhaCorreta = "secreto";
+
+let j = 0;
+let acessoLiberado = false;
+
+while (j < tentativas.length && j < 3) {
+    if (tentativas[j] === senhaCorreta) {
+        console.log("Acesso permitido!");
+        acessoLiberado = true;
+        break;
+    } else {
+        console.log(`Tentativa ${j + 1} inválida.`);
+    }
+    j++;
+}
+
+if (!acessoLiberado) {
+    console.log("Acesso bloqueado. Número máximo de tentativas atingido.");
 }
