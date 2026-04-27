6576b486b98cd0adbdb3c4373c2cf7265b47bf62
Carol <carolinevictoria1224@gmail.com>
2026-04-27 15:09:27 -0300

    Consertando bugs e adicionando restante dos metodos de inserção de tarefa

--- projeto_fokus/script-crud.js
+++ projeto_fokus/script-crud.js
@@ -13,17 +13,17 @@ function criarElementoTarefa(tarefa) {
     
     const svg = document.createElement('svg')
     svg.innerHTML = `
-    <svg>
         <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
         </svg>
-    </svg>
     `
     const paragrafo = document.createElement('p')
     paragrafo.textContent = tarefa.descicao
+    paragrafo.classList.add('app__section-task-list-item-description')
 
     const button = document.createElement('button')
+    button.classList.add('app_button-edit')
     const imagemBotao = document.createElement('img')
     imagemBotao.setAttribute('src', 'imagens/edit.png')
 
@@ -44,7 +44,11 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
         descicao: textarea.value
     }
     tarefas.push(tarefa)
+    const elementoTarefa = criarElementoTarefa(tarefa)
+    ulTarefas.append(elementoTarefa)
     localStorage.setItem('tarefas', JSON.stringify(tarefas))
+    textarea.value = ''
+    formAdicionarTarefa.classList.add('hidden')
 })
 
 tarefas.forEach(tarefa =>{
