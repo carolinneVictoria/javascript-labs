f11574585c738ad59ade2acd8486814f90d3e718
Carol <carolinevictoria1224@gmail.com>
2026-04-27 14:27:19 -0300

    Adicionando tarefas na lista

--- projeto_fokus/script-crud.js
+++ projeto_fokus/script-crud.js
@@ -1,7 +1,19 @@
 //encontrar o botao de adicionar
+
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
+const textarea = document.querySelector('.app__form-textarea')
+const tarefas = []
 
 btnAdicionarTarefa.addEventListener('click', () => {
     formAdicionarTarefa.classList.toggle('hidden')
+})
+
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    const tarefa = {
+        descicao: textarea.value
+    }
+    tarefas.push(tarefa)
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
 })
