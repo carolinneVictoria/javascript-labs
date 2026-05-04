b5335e9ce1d44e5168f2260cf170d9f57e1bf819
Carol <carolinevictoria1224@gmail.com>
2026-05-04 13:56:04 -0300

    Exercicio total de despesas

--- praticas/arrays.js
+++ praticas/arrays.js
@@ -1,5 +1,14 @@
 // Lista de Compras
 let listaCompras = ['arroz', 'macarrão', 'óleo']
 console.log("Segundo item da lista: " + listaCompras[1])
-listaDeCompras[listaDeCompras.length - 1] = 'feijão'
+listaCompras[listaCompras.length - 1] = 'feijão'
 console.log("Lista de Compras após modificação: " + listaCompras)
+
+// Total de despesas
+const despesas = [120, 80, 45.5, 200, 60];
+let soma = 0;
+
+for (let i = 0; i < despesas.length; i++) {
+    soma += despesas[i]
+}
+console.log(`Total de despesas: R$ ${soma}`);
