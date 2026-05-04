2ef181aeeb531fc6dfa7e75edccc70f55cc0db07
Carol <carolinevictoria1224@gmail.com>
2026-05-04 13:46:40 -0300

    Exercicio Lista de Compras

---
+++ praticas/arrays.js
@@ -0,0 +1,5 @@
+// Lista de Compras
+let listaCompras = ['arroz', 'macarrão', 'óleo']
+console.log("Segundo item da lista: " + listaCompras[1])
+listaDeCompras[listaDeCompras.length - 1] = 'feijão'
+console.log("Lista de Compras após modificação: " + listaCompras)
