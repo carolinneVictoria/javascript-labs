5fb7bf3a7f49d30456ae7e90b33448fb986bbbbd
Carol <carolinevictoria1224@gmail.com>
2026-05-04 14:01:34 -0300

    Exercicio com foreach e for of

--- praticas/arrays.js
+++ praticas/arrays.js
@@ -12,3 +12,15 @@ for (let i = 0; i < despesas.length; i++) {
     soma += despesas[i]
 }
 console.log(`Total de despesas: R$ ${soma}`);
+
+// Exibindo nomes com for of
+const estudantes = ['Carla', 'João', 'Maria', 'Lucas'];
+for (const nome of estudantes){
+    console.log("Estudante: ", nome);
+}
+
+// Exibindo mensagem com forEach
+const mensagens = ['Pedido confirmado', 'Pagamento aprovado', 'Produto enviado'];
+mensagens.forEach(msg => {
+    console.log("Notificação:", msg);
+});
