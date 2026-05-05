eb5d7b899607f7968f3c4c485074011b854c8a01
Carol <carolinevictoria1224@gmail.com>
2026-05-05 16:33:58 -0300

    Create api.js

---
+++ projeto_adoPet/js/api.js
@@ -0,0 +1,15 @@
+const api = {
+
+    async buscarPets(){
+        try {
+            const response = await fetch('http://localhost:3000/pets')
+            return await response.json()
+
+        } catch (error) {
+            alert('Erro ao buscar dados! :( ')
+            throw error
+        }
+    }
+}
+
+export default api;
