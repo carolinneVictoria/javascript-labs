f45d77a51fe2d51aac8e0206ce9e9122e63ea52e
Carol <carolinevictoria1224@gmail.com>
2026-05-06 14:27:38 -0300

    Update api.js

--- projeto_adoPet/js/api.js
+++ projeto_adoPet/js/api.js
@@ -25,6 +25,33 @@ const api = {
             alert('Erro ao salvar novo pet! :( ')
             throw error;
         }
+    },
+
+    async buscarPetPorId(id){
+        try {
+            const response = await fetch(`http://localhost:3000/pets/${id}`)
+            return await response.json()
+
+        } catch (error) {
+            alert('Erro ao buscar pet! :( ')
+            throw error
+        }
+    },
+
+    async editarPet(pet){
+        try {
+            const response = await fetch(`http://localhost:3000/pets/${pet.id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify(pet)
+        })
+        return await response.json()
+        } catch (error) {
+            alert('Erro ao salvar novo pet! :( ')
+            throw error;
+        }
     }
 
 }
