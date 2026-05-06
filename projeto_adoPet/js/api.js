5989e36a8037aae1899f0be49df69b81594156e7
Carol <carolinevictoria1224@gmail.com>
2026-05-06 11:30:19 -0300

    Update api.js

--- projeto_adoPet/js/api.js
+++ projeto_adoPet/js/api.js
@@ -9,7 +9,24 @@ const api = {
             alert('Erro ao buscar dados! :( ')
             throw error
         }
+    },
+
+    async salvarPet(pet){
+        try {
+            const response = await fetch('http://localhost:3000/pets', {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify(pet)
+        })
+        return await response.json()
+        } catch (error) {
+            alert('Erro ao salvar novo pet! :( ')
+            throw error;
+        }
     }
+
 }
 
 export default api;
