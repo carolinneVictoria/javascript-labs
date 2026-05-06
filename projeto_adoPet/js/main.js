78fa72f2e42b169796a63345b71fd0a4b34b7432
Carol <carolinevictoria1224@gmail.com>
2026-05-06 11:30:21 -0300

    Update main.js

--- projeto_adoPet/js/main.js
+++ projeto_adoPet/js/main.js
@@ -1,5 +1,36 @@
 import ui from "./ui.js";
+import api from "./api.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     ui.renderizarPets()
+
+    const botaoCancelar = document.getElementById("botao-cancelar")
+    const formularioPet = document.getElementById("cadastro-pet-form")
+
+    formularioPet.addEventListener("submit", manipularSubmissaoFormulario)
+    botaoCancelar.addEventListener("click", manipularCancelamento);
+
+    function manipularCancelamento(){
+        ui.limparFormulario();
+    }
+    function manipularSubmissaoFormulario(event){
+        event.preventDefault();
+        const especie = document.getElementById("pet-especie").value
+        const nome    = document.getElementById("pet-nome").value
+        const raca    = document.getElementById("pet-raca").value
+
+        if (!nome || !especie || !raca) {
+            alert('Preencha todos os campos!');
+            return;
+        }
+
+        api.salvarPet({especie, nome, raca})
+            .then(() => {
+                ui.renderizarPets()
+                ui.limparFormulario();
+            })
+            .catch(error => {
+                alert('Erro ao salvar pet! :(')
+            })
+    }
 })
