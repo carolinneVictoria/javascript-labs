22de362d64b53cff08f9698eead34f6a16e4b025
Carol <carolinevictoria1224@gmail.com>
2026-05-06 11:30:24 -0300

    Update ui.js

--- projeto_adoPet/js/ui.js
+++ projeto_adoPet/js/ui.js
@@ -3,20 +3,42 @@ import api from "./api.js";
 const ui = {
     async renderizarPets(){
         const listaPets = document.getElementById('lista-pets')
-        try {
+        
+        try{
             const pets = await api.buscarPets()
-            pets.forEach((pet => {
-                listaPets.innerHTML += `
-                    <li class="li-pet" data-id="${pet.id}">
-                    <div class="pet-especie">${pet.especie}</div>
-                    <div class="pet-nome">${pet.nome}</div>
-                    <div class="pet-raca">${pet.raca}</div>
-                    </li>
-                `
-            }))
+            pets.forEach(pet => ui.adicionarPetNaLista(pet))
+
         } catch (error) {
             alert('Erro ao tentar renderizar os pets! :( ')
         }
+    },
+
+    adicionarPetNaLista(pet){
+        const listaPets = document.getElementById("lista-pets")
+        const li = document.createElement("li")
+        li.setAttribute("data-id", pet.id)
+        li.classList.add("li-pet")
+
+        const petEspecie = document.createElement("div")
+        petEspecie.textContent = pet.especie
+        petEspecie.classList.add("pet-especie")
+
+        const petNome = document.createElement("div")
+        petNome.textContent = pet.nome
+        petNome.classList.add("pet-nome")
+
+        const petRaca = document.createElement("div")
+        petRaca.textContent = pet.raca
+        petRaca.classList.add("pet-raca")
+
+        li.appendChild(petEspecie)
+        li.appendChild(petNome)
+        li.appendChild(petRaca)
+        listaPets.appendChild(li)
+    },
+
+    limparFormulario(){
+        document.getElementById("cadastro-pet-form").reset();
     }
 }
 export default ui;
