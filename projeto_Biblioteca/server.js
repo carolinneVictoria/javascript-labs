77c86c92d01433e62db7f887410f68c8fb055e5b
Carol <carolinevictoria1224@gmail.com>
2026-06-25 14:13:53 -0300

    fix: deletando pasta

--- projeto_Biblioteca/server.js
+++
@@ -1,9 +0,0 @@
-// Servidor HTTP Local
-import "dotenv/config";
-import app from "./src/app.js";
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    console.log(`Servidor escutando na porta ${PORT}!`);
-});
