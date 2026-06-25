751c11fd5055e9818d8f4972193b915246491bb7
Carol <carolinevictoria1224@gmail.com>
2026-06-25 11:54:20 -0300

    feat: adicionando paginacao reutilizavel

--- api-node-express-main/src/controllers/livrosController.js
+++ api-node-express-main/src/controllers/livrosController.js
@@ -1,28 +1,13 @@
 import NaoEncontrado from "../erros/NaoEncontrado.js";
-import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js"
 import {autores, livros} from "../models/index.js";
 
 class LivroController {
 
   static listarLivros = async (req, res, next) => {
     try {
-      let { limite = 5, pagina = 1} = req.query;
-
-      limite = parseInt(limite);
-      pagina = parseInt(pagina);
-
-      if(limite > 0 && pagina > 0){
-        const livrosResultado = await livros.find()
-          .skip((pagina - 1) * limite)
-          .limit(limite)
-          .populate("autor")
-          .exec();
-  
-        res.status(200).json(livrosResultado);
-      } else {
-        next(new RequisicaoIncorreta());
-      }
-
+      const buscaLivros = livros.find();
+      req.resultado = buscaLivros;
+      next();
     } catch (erro) {
       next(erro);
     }
@@ -95,8 +80,9 @@ class LivroController {
       const busca = await processaBusca(req.query);
 
       if (busca !== null) {
-        const livrosResultado = await livros.find(busca).populate("autor");
-        res.status(200).send(livrosResultado);
+        const livrosResultado = livros.find(busca).populate("autor");
+        req.resultado = livrosResultado;
+        next();
       } else {
         res.status(200).send([])
       }
