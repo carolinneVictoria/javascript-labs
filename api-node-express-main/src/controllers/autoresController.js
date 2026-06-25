911a85350d660c760bf980884bbf46e19b720c79
Carol <carolinevictoria1224@gmail.com>
2026-06-25 11:54:16 -0300

    feat: adicionando paginacao reutilizavel

--- api-node-express-main/src/controllers/autoresController.js
+++ api-node-express-main/src/controllers/autoresController.js
@@ -5,10 +5,9 @@ class AutorController {
 
   static listarAutores = async (req, res, next) => {
     try {
-      const autoresResultado = await autores.find();
-
-      res.status(200).json(autoresResultado);
-
+      const buscaAutor = autores.find();
+      req.resultado = buscaAutor;
+      next();
     } catch (erro) {
       next(erro);
     }
