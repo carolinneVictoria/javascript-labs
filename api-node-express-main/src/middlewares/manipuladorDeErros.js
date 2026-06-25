7ec25b9c033806cec938a33169ac0a0cb806ffba
Carol <carolinevictoria1224@gmail.com>
2026-06-25 11:27:25 -0300

    feat: paginando uma rota

--- api-node-express-main/src/middlewares/manipuladorDeErros.js
+++ api-node-express-main/src/middlewares/manipuladorDeErros.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 import ErroBase from "../erros/ErroBase.js";
 import ErroValidacao from "../erros/ErroValidacao.js";
-import NaoEncontrado from "../erros/NaoEncontrado.js";
 import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
 
 function manipuladorDeErros(erro, req, res, next) {
@@ -9,7 +8,7 @@ function manipuladorDeErros(erro, req, res, next) {
         new RequisicaoIncorreta().enviarResposta(res);
     } else if (erro instanceof mongoose.Error.ValidationError) {
         new ErroValidacao(erro).enviarResposta(res);
-    } else if (erro instanceof NaoEncontrado) {
+    } else if (erro instanceof ErroBase) {
         erro.enviarResposta(res);
     } else {
         new ErroBase().enviarResposta(res);
