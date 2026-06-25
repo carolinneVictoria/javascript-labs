d7bc8ebcea0c9f24cbbd213f1245d06bcccf9b9f
Carol <carolinevictoria1224@gmail.com>
2026-06-24 22:08:03 -0300

    feat: adicionando validacoes do moongose

--- api-node-express-main/src/models/Livro.js
+++ api-node-express-main/src/models/Livro.js
@@ -2,14 +2,24 @@ import mongoose from "mongoose";
 
 const livroSchema = new mongoose.Schema(
   {
-    id: {type: String},
-    titulo: {type: String, required: [true, "O titulo do livro é obrigatório!"]},
-    autor: {type: mongoose.Schema.Types.ObjectId, ref: 'autores', required: [true, "O(a) autor(a) é obrigatório!"]},
-    editora: {type: String, required: [true, "A editora é obrigatório!"]},
-    numeroPaginas: {type: Number}
+    id: { type: String },
+    titulo: { type: String, required: [true, "O titulo do livro é obrigatório!"] },
+    autor: { type: mongoose.Schema.Types.ObjectId, ref: 'autores', required: [true, "O(a) autor(a) é obrigatório!"] },
+    editora: { 
+      type: String,
+      required: [true, "A editora é obrigatório!"],
+      enum: {
+        values: ["Casa do Código", "Alura"],
+        message: "A editora {VALUE} não é um valor permitido."
+      }
+    },
+    numeroPaginas: {
+      type: Number,
+      min: [10, "O número de paginas deve estar entre 10 e 5000. Valor fonecido: {VALUE}"],
+      max: [5000, "O número de paginas deve estar entre 10 e 5000. Valor fonecido: {VALUE}"] }
   }
 );
 
-const livros= mongoose.model('livros', livroSchema);
+const livros = mongoose.model('livros', livroSchema);
 
 export default livros;
