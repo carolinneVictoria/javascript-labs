8ce4b9e3cf3333d6da25265887034579becdc7f6
Carol <carolinevictoria1224@gmail.com>
2026-06-27 18:37:01 -0300

    Create app.js

---
+++ 2-praticas/7-days-of-code/variaveis/app.js
@@ -0,0 +1,12 @@
+const formulario = document.querySelector("#infoForm");
+
+formulario.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const nome      = document.getElementById("nome").value;
+    const idade     = document.getElementById("idade").value;
+    const linguagem = document.getElementById("linguagem").value;
+
+    
+
+})
