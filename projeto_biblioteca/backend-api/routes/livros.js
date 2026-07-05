75e15de4d14b78bcaec89c375a0b138d88b82c38
Carol <carolinevictoria1224@gmail.com>
2026-07-05 20:54:03 -0300

    feat: adicionando novas rotas e tratamentos de erro

--- projeto_biblioteca/backend-api/routes/livros.js
+++ projeto_biblioteca/backend-api/routes/livros.js
@@ -3,7 +3,26 @@ const { Router } = require('express');
 const router = Router();
 
 router.get("/", (req, res) => {
-    res.send('Estou no github!');
+    try {
+        // throw new Error('Erro de teste'); -- Para testar o tratamento de erro
+        res.send('Olá mundo!');
+    } catch (error) {
+        res.status(500).send('Ocorreu um erro ao processar a requisição.');
+    }
 })
+
+router.post("/", (req, res) => {
+    res.send('Recebido, voce fez uma requisição do tipo Post!');
+})
+
+router.patch("/", (req, res) => {
+    res.send('Recebido, voce fez uma requisição do tipo Patch!');
+})
+
+router.delete("/", (req, res) => {
+    res.send('Recebido, voce fez uma requisição do tipo Delete!');
+})
+
+
 
 module.exports = router;
