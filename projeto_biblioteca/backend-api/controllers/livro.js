5f436ee791362795e72f63be7151d2b3f5b3c7fa
Carol <carolinevictoria1224@gmail.com>
2026-07-05 21:24:19 -0300

    feat: usando fs para manipulacao de JSON

--- projeto_biblioteca/backend-api/controllers/livro.js
+++ projeto_biblioteca/backend-api/controllers/livro.js
@@ -1,7 +1,10 @@
+const fs = require('fs');
+
 function getLivros (req, res) {
     try {
         // throw new Error('Erro de teste'); -- Para testar o tratamento de erro
-        res.send('Olá mundo!');
+        const livros = JSON.parse(fs.readFileSync('../livros.json', 'utf-8'));
+        res.send(livros);
     } catch (error) {
         res.status(500).send('Ocorreu um erro ao processar a requisição.');
     }
