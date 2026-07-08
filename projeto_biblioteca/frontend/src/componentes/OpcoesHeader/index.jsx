f8b9f7ea2600c3be1ca6b3ca390654333904c57e
Carol <carolinevictoria1224@gmail.com>
2026-07-07 22:40:00 -0300

    feat: adicionando rotas na aplicacao

--- projeto_biblioteca/frontend/src/componentes/OpcoesHeader/index.jsx
+++ projeto_biblioteca/frontend/src/componentes/OpcoesHeader/index.jsx
@@ -1,15 +1,17 @@
+import { Link } from "react-router-dom"
+
 const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
 
 function OpcoesHeader() {
     return (
-        <div className="flex">
+        <div className="flex gap-4.5">
             {textoOpcoes.map((texto) => (
                 <div
                     key={texto}
                     className="text-base flex justify-center items-center text-center
                         h-full px-[5px] cursor-pointer min-h-30"
                 >
-                    <p>{texto}</p>
+                    <Link to={`/${texto.toLowerCase()}`}><p>{texto}</p></Link>
                 </div>
             ))}
         </div>
---
+++ projeto_biblioteca/frontend/src/rotas/Favoritos.jsx
@@ -0,0 +1,11 @@
+import Pesquisa from "../componentes/Pesquisa";
+
+function Favoritos() {
+  return (
+    <div className="w-screen h-screen bg-[linear-gradient(90deg,#002F52_35%,#326589_165%)]">
+      <Pesquisa />
+    </div>
+  );
+}
+
+export default Favoritos;
