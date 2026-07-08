236259da9217dae04e29f6d83d275bad273edb89
Carol <carolinevictoria1224@gmail.com>
2026-07-08 13:17:10 -0300

    feat: adicionando cors para rotas

--- projeto_biblioteca/backend-api/app.js
+++ projeto_biblioteca/backend-api/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const rotaLivros = require('./routes/livros');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json()); // Middleware para habilitar o parsing de JSON no corpo das requisições
+app.use(cors({ origin: '*' })); // Middleware para habilitar o CORS
 
 const port = 8000;
 
